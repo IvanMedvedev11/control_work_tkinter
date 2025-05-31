8eb21336770cc131ab0ac0b5446d46b901b890b3
Median72 <medvivan11@gmail.com>
2025-05-31 18:08:04 +0700

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,25 @@
+from tkinter import *
+def show_up(event):
+    global lbl
+    lbl['text'] = "верх"
+
+def show_down(event):
+        global lbl
+        lbl['text'] = "низ"
+
+def show_left(event):
+            global lbl
+            lbl['text'] = "лево"
+
+
+def show_right(event):
+    global lbl
+    lbl['text'] = "право"
+root = Tk()
+lbl = Label(root)
+lbl.place(x=100, y=100)
+root.bind('<Up>', show_up)
+root.bind('<Down>', show_down)
+root.bind('<Left>', show_left)
+root.bind('<Right>', show_right)
+root.mainloop()
